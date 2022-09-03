1fbaf87684ba8a656178543624c10070cee80641
Wilmer <wilmernorling@gmail.com>
2022-09-03 15:10:16 +0200

    Pressed key will now show up on screen

--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
-console.log("hello")
+const keys = document.querySelectorAll(".key")
+const screen = document.querySelector(".screen")
+screen.textContent = ""
+keys.forEach(key => {
+    key.addEventListener("click", function(e){
+        if(e.target.innerText == "Clear"){
+            screen.textContent = ""
+        }else if(e.target.innerText == "+" || e.target.innerText == "/" || e.target.innerText == "-" || e.target.innerText == "*"){
+            screen.textContent += " " + e.target.innerText + " "
+        }
+        else{
+            screen.textContent += e.target.innerText
+        }
+    })
+});
+
+
+
+
+
---
+++ readMe.md
@@ -0,0 +1,12 @@
+Calculator
+    Values inputed by user is going to be calculated using "pamdas" in correct order
+
+Problems:
+    getting the values
+        each button correlates to a number wich will be pushed into a string
+    
+    calculating
+        will take every thing the user inputed and calculate it
+        once = is pressed user will have to AC for it to input values again
+    display
+        will display input 
