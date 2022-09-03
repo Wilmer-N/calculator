e3446b486736de09bc8b1c67dcf8156f03f0f2cb
Wilmer <wilmernorling@gmail.com>
2022-09-03 16:47:11 +0200

    I can now do addition

--- javascript.js
+++ javascript.js
@@ -1,19 +1,49 @@
 const keys = document.querySelectorAll(".key")
 const screen = document.querySelector(".screen")
+num = ""
+num1 = 0
+ans = 0
+let array = []
 screen.textContent = ""
+
 keys.forEach(key => {
     key.addEventListener("click", function(e){
-        if(e.target.innerText == "Clear"){
-            screen.textContent = ""
-        }else if(e.target.innerText == "+" || e.target.innerText == "/" || e.target.innerText == "-" || e.target.innerText == "*"){
-            screen.textContent += " " + e.target.innerText + " "
+        let input = e.target.innerText
+        textDisplay(e)
+       if(Number.isInteger(parseInt(input))){
+        num = num + parseInt(input)
+       }else{
+        if(num1){
+            num2 = parseInt(num)
+            console.log(addition(num1, num2))
+            num1 = addition(num1, num2)
+        }else{
+        num1 = parseInt(num)
+        console.log(num1)
         }
-        else{
-            screen.textContent += e.target.innerText
-        }
+        num = ""
+       }
     })
 });
 
+
+
+function addition(num1, num2){
+    return num1 + num2
+}
+
+function textDisplay(e){
+    let input = e.target.innerText
+    if(input == "Clear"){
+        screen.textContent = ""
+        globalThis.num1 = ""
+    }else if(input == "+" || input == "/" || input == "-" || input == "*"){
+        screen.textContent += " " + input + " "
+    }
+    else{
+        screen.textContent += input
+    }
+}
 
 
 
